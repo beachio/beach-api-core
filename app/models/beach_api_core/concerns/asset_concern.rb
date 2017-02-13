6662626feb575cac8b407a8242f091c478a7a5e0
Kate <katerina_karpycheva@mail.ru>
2017-02-13 20:28:26 +0200

    Fixed specs

--- app/models/beach_api_core/concerns/asset_concern.rb
+++ app/models/beach_api_core/concerns/asset_concern.rb
@@ -2,6 +2,7 @@ module BeachApiCore::Concerns::AssetConcern
   extend ActiveSupport::Concern
 
   def file_blank?(attrs)
+    return false if attrs['base64']
     file = attrs['file']
     file = Refile.parse_json(file, symbolize_names: true) if file.is_a?(String)
     file.blank?
