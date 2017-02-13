67745d32e7090e2974e6fc26cf29ae519f1507e1
Kate <katerina_karpycheva@mail.ru>
2017-02-14 00:40:45 +0200

    Added basic seeds

--- app/models/beach_api_core/user.rb
+++ app/models/beach_api_core/user.rb
@@ -29,6 +29,14 @@ module BeachApiCore
     before_validation :generate_profile, on: :create
     before_validation :generate_username
 
+    class << self
+      [:admin, :developer].each do |role|
+        define_method "#{role}s" do
+          where(id: BeachApiCore::Assignment.where(role: BeachApiCore::Role.send(role)).select(:user_id))
+        end
+      end
+    end
+
     private
 
     def generate_profile
---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+BeachApiCore::Role.destroy_all
+BeachApiCore::Instance.destroy_all
+BeachApiCore::User.destroy_all
+Doorkeeper::Application.destroy_all
+
+# Create instance record
+BeachApiCore::Instance.current
+
+# Create basic roles
+[:admin, :developer].each do |name|
+  role = BeachApiCore::Role.create(name: name)
+  user = BeachApiCore::User.create(email: Faker::Internet.email, password: Faker::Internet.password)
+  BeachApiCore::Assignment.create(role: role, keeper: BeachApiCore::Instance.current, user: user)
+end
+
+Doorkeeper::Application.create(name: Faker::Company.name, redirect_uri: Faker::Internet.redirect_uri,
+                               owner: BeachApiCore::User.developers.first)
