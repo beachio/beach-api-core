4593490e0b64bc79e156cc0673a56c6c7d4ed7fc
Kate <katerina_karpycheva@mail.ru>
2017-02-14 12:50:09 +0200

    Changed organisation membership for user

--- app/models/beach_api_core/user.rb
+++ app/models/beach_api_core/user.rb
@@ -16,16 +16,18 @@ module BeachApiCore
 
     has_many :invitations
 
-    has_one :organisation_membership, as: :member, class_name: 'BeachApiCore::Membership', dependent: :destroy
+    has_one :organisation_membership, -> { where(group_type: 'BeachApiCore::Organisation') }, as: :member, inverse_of: :member,
+            class_name: 'BeachApiCore::Membership', dependent: :destroy
     has_one :organisation, through: :organisation_membership, source: :group, source_type: 'BeachApiCore::Organisation'
     has_many :memberships, -> { where(group_type: 'BeachApiCore::Team') }, as: :member, inverse_of: :member, dependent: :destroy
-    accepts_nested_attributes_for :memberships, allow_destroy: true
     has_many :teams, through: :memberships, source: :group, source_type: 'BeachApiCore::Team'
 
     has_secure_password
     acts_as_downcasable_on [:email, :username]
 
     accepts_nested_attributes_for :profile
+    accepts_nested_attributes_for :organisation_membership, allow_destroy: true, reject_if: proc { |attr| attr[:group_id].blank? }
+    accepts_nested_attributes_for :memberships, allow_destroy: true
 
     before_validation :generate_profile, on: :create
     before_validation :generate_username
