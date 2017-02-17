baa9202eda4ca804f158df52f1d10a64568fc892
Kate <katerina_karpycheva@mail.ru>
2017-02-17 12:44:30 +0200

    Fixed params in docs for finding group

--- app/docs/invitations_doc.rb
+++ app/docs/invitations_doc.rb
@@ -6,9 +6,9 @@ module InvitationsDoc
   header 'HTTP_AUTHORIZATION', 'Bearer access_token', required: true
   param :invitation, Hash, required: true do
     param :email, String, required: true
-    param :group_type, %w(Team Organisation), required: true
-    param :group_id, String, required: true
   end
+  param :group_type, %w(Team Organisation), required: true
+  param :group_id, String, required: true
   example "\"invitation\": #{apipie_invitation_response} \nfail: 'Errors Description'"
   def create
   end
--- app/docs/memberships_doc.rb
+++ app/docs/memberships_doc.rb
@@ -7,10 +7,10 @@ module MembershipsDoc
   param :membership, Hash, required: true do
     param :member_id, String, required: true
     param :member_type, %w(Team User), required: true
-    param :group_id, String, required: true
-    param :group_type, %w(Team Organisation), required: true
     param :owner, [true, false]
   end
+  param :group_id, String, required: true
+  param :group_type, %w(Team Organisation), required: true
   example "\"team(or organisation)\": #{apipie_team_response} \nfail: 'Errors Description'"
   def create
   end
