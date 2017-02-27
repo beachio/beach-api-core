5da38572df0ae9f01391145503a6031d4fc09581
Kate <katerina_karpycheva@mail.ru>
2017-02-27 14:00:08 +0200

    Fixed organisation membership validation

--- app/models/beach_api_core/membership.rb
+++ app/models/beach_api_core/membership.rb
@@ -2,17 +2,18 @@ module BeachApiCore
   class Membership < ApplicationRecord
     validates :group_id, uniqueness: { scope: [:group_type, :member_id, :member_type] }
     validates :member, :group, presence: true
-    validate :member_can_consist_only_in_one_organization
+    validate :member_can_join_only_one_organization
 
     belongs_to :member, polymorphic: true
     belongs_to :group, polymorphic: true
 
     private
 
-    def member_can_consist_only_in_one_organization
+    def member_can_join_only_one_organization
       errors.add(:member, 'can not join more than one organization') if member &&
           group.is_a?(BeachApiCore::Organisation) &&
-          member.organisation.present?
+          member.organisation.present? &&
+          member.organisation != group
     end
   end
 end
