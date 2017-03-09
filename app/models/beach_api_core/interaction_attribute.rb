55e3250ec5174ba625ee0b8217d778984ef53123
Kate <katerina_karpycheva@mail.ru>
2017-03-09 16:57:02 +0200

    Changed autosave to touch

--- app/models/beach_api_core/interaction_attribute.rb
+++ app/models/beach_api_core/interaction_attribute.rb
@@ -1,6 +1,6 @@
 module BeachApiCore
   class InteractionAttribute < ApplicationRecord
-    belongs_to :interaction, inverse_of: :interaction_attributes, class_name: 'BeachApiCore::Interaction', autosave: true
+    belongs_to :interaction, inverse_of: :interaction_attributes, class_name: 'BeachApiCore::Interaction', touch: true
 
     validates :key, presence: true
   end
