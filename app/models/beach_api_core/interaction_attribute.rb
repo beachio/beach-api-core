7a061acbb8c22aeba6f3f2e24a59c7c8423819f4
Kate <katerina_karpycheva@mail.ru>
2017-03-09 12:19:07 +0200

    Added autosave for interaction_attributes

--- app/models/beach_api_core/interaction_attribute.rb
+++ app/models/beach_api_core/interaction_attribute.rb
@@ -1,6 +1,6 @@
 module BeachApiCore
   class InteractionAttribute < ApplicationRecord
-    belongs_to :interaction, inverse_of: :interaction_attributes, class_name: 'BeachApiCore::Interaction'
+    belongs_to :interaction, inverse_of: :interaction_attributes, class_name: 'BeachApiCore::Interaction', autosave: true
 
     validates :key, presence: true
   end
