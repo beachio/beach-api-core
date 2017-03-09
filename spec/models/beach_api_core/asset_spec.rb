ab7965512a36ca561680be4346dfff58461343d6
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2017-03-09 18:56:35 +0000

    [170] Ability to provide asset filename

--- spec/models/beach_api_core/asset_spec.rb
+++ spec/models/beach_api_core/asset_spec.rb
@@ -16,11 +16,34 @@ module BeachApiCore
 
     it 'should grab file extension' do
       [:jpg, :jpeg, :bmp, :png, :gif, :doc, :docx, :xls, :xlsx, :pdf, :txt].each do |extension|
-        asset = create :asset, file_filename: "filename.#{extension}"
+        asset = create :asset,
+                       file: Refile::FileDouble.new('dummy', 'logo.png', content_type: 'image/png'),
+                       file_filename: "filename.#{extension}"
         expect(asset.file_extension).to eq extension.to_s
       end
       expect(Asset.images.count).to eq 5
       expect(Asset.files.count).to eq 6
+    end
+
+    context 'asset with base64' do
+      let(:base64) { "data:image/png;base64,#{Base64.encode64(File.read('spec/uploads/test.png'))}" }
+      it 'should create' do
+        expect { create :asset, base64: base64 }.to change(BeachApiCore::Asset, :count).by(1)
+      end
+
+      context 'filename' do
+        it 'should generate filename' do
+          asset = create(:asset, base64: base64)
+          expect(asset.file_filename).to eq 'asset.png'
+        end
+
+        it 'should store provided filename' do
+          filename = Faker::File.file_name
+          asset = create(:asset, base64: base64, name: filename)
+          expect(asset.file_filename).to eq filename
+        end
+
+      end
     end
 
   end
