12b8b4d0aec00a79abbec38d7c60ed895ea3618a
andy <mailto.alekseenko@gmail.com>
2017-03-14 16:05:05 +0000

    Added common elasticsearch indexer and indexable concern for models; added elasticsearch on-off switch to engine; updated readme

---
+++ app/models/beach_api_core/concerns/elasticsearch_indexable.rb
@@ -0,0 +1,32 @@
+module BeachApiCore::Concerns::ElasticsearchIndexable
+  extend ActiveSupport::Concern
+
+  included do
+    include ::Elasticsearch::Model
+
+    after_commit :update_index, on: [:create, :update], if: '::BeachApiCore::Engine.elasticsearch_enabled'
+    after_commit :destroy_index, on: :destroy, if: '::BeachApiCore::Engine.elasticsearch_enabled'
+
+    private
+
+    class << self
+      def elasticsearch_serializer(klass_name)
+        @_elasticserach_serializer = klass_name.constantize
+      end
+
+      def elasticserach_serializer_value
+        @_elasticserach_serializer
+      end
+    end
+
+    def update_index
+      opts = {}
+      opts[:serializer] = self.class.elasticserach_serializer_value if self.class.elasticserach_serializer_value
+      ::BeachApiCore::ElasticsearchIndexer.perform_async(self.class, id, opts)
+    end
+
+    def destroy_index
+      ::BeachApiCore::ElasticsearchIndexer.perform_async(self.class, id, action: 'delete')
+    end
+  end
+end
---
+++ app/workers/beach_api_core/elasticsearch_indexer.rb
@@ -0,0 +1,32 @@
+class BeachApiCore::ElasticsearchIndexer
+  include ::Sidekiq::Worker
+  sidekiq_options queue: 'elasticsearch'
+  sidekiq_options retry: 10
+
+  # Available options are:
+  # - action: index or delete
+  # - serializer: serializer class to use instead of "as_indexed_json"
+  def perform(klass, id, opts = {})
+    return unless (entity = klass.find_by(id: id))
+
+    opts.symbolize_keys!
+    opts[:action] ||= 'index'
+    elastic_opts = { index: klass.index_name, type: klass.document_type, id: entity.id }
+    client = klass.__elasticsearch__.client
+
+    case opts[:action]
+    when 'index'
+      client.index elastic_opts.merge(body: serialized_entity(entity, opts[:serializer]))
+    when 'delete'
+      client.delete elastic_opts
+    else raise ArgumentError, "Unknown action '#{opts[:action]}'"
+    end
+  end
+
+  private
+
+  def serialized_entity(entity, serializer = nil)
+    return entity.as_indexed_json unless serializer
+    serializer.new(entity).as_json
+  end
+end
