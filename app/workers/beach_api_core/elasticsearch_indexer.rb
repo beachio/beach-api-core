2f12ce597b946684129b67d0876852128b31308a
andy <mailto.alekseenko@gmail.com>
2017-03-15 13:33:31 +0000

    Added constantize to all class names in elasticsearch indexer

--- app/workers/beach_api_core/elasticsearch_indexer.rb
+++ app/workers/beach_api_core/elasticsearch_indexer.rb
@@ -7,6 +7,7 @@ class BeachApiCore::ElasticsearchIndexer
   # - action: index or delete
   # - serializer: serializer class to use instead of "as_indexed_json"
   def perform(klass, id, opts = {})
+    klass = klass.constantize
     return unless (entity = klass.find_by(id: id))
 
     opts.symbolize_keys!
@@ -27,6 +28,6 @@ class BeachApiCore::ElasticsearchIndexer
 
   def serialized_entity(entity, serializer = nil)
     return entity.as_indexed_json unless serializer
-    serializer.new(entity).as_json
+    serializer.constantize.new(entity).as_json
   end
 end
