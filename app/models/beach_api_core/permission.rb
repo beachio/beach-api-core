c0ae33959cd4d90bf70edad50e753f18735af22a
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2017-03-20 08:45:52 +0000

    [90] Support methods for AA

--- app/models/beach_api_core/permission.rb
+++ app/models/beach_api_core/permission.rb
@@ -7,5 +7,21 @@ module BeachApiCore
     validates :actions, presence: true, allow_blank: true
     validates :atom, uniqueness: { scope: [:keeper_id, :keeper_type] }
     validates :keeper_type, inclusion: %w(BeachApiCore::Role BeachApiCore::Team BeachApiCore::User)
+
+    %w(create read update delete execute).each do |action|
+      define_method action do
+        actions[action]
+      end
+
+      define_method "#{action}=" do |value|
+        actions[action] = value
+      end
+    end
+
+    def keeper_name
+      return unless keeper
+      "#{keeper.try(:email) || keeper.try(:name)} (#{keeper_type}, #{keeper_id})"
+    end
+
   end
 end
