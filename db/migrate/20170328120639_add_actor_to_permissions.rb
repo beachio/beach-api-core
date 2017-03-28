aa94e2b6825f985837afaa4c427b2b6c5515bed5
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2017-03-28 20:19:58 +0000

    Add migration

---
+++ db/migrate/20170328120639_add_actor_to_permissions.rb
@@ -0,0 +1,7 @@
+class AddActorToPermissions < ActiveRecord::Migration[5.0]
+  def change
+    add_column :beach_api_core_permissions, :actor, :string
+    add_index :beach_api_core_permissions, [:atom_id, :actor, :keeper_id, :keeper_type], unique: true,
+              name: 'index_bac_permissions_on_atom_id_and_actor_and_k_id_and_k_type'
+  end
+end
