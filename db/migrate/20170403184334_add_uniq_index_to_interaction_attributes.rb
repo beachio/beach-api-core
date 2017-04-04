a9ff84d2d2d7cc15544e552f43b765a58fb9caa4
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2017-04-04 09:49:19 +0000

    Improve migrate

--- db/migrate/20170403184334_add_uniq_index_to_interaction_attributes.rb
+++ db/migrate/20170403184334_add_uniq_index_to_interaction_attributes.rb
@@ -1,5 +1,12 @@
 class AddUniqIndexToInteractionAttributes < ActiveRecord::Migration[5.0]
   def change
+    BeachApiCore::InteractionAttribute.select('interaction_id,key')
+        .group('interaction_id,key').having('count(interaction_id) > 1').each do |interaction_attribute|
+      interaction_attributes = BeachApiCore::InteractionAttribute.where(interaction_id: interaction_attribute.interaction_id,
+                                                                        key: interaction_attribute.key).to_a
+      interaction_attributes.pop
+      interaction_attributes.each(&:destroy)
+    end
     add_index :beach_api_core_interaction_attributes, [:interaction_id, :key],
               unique: true, name: 'index_bac_interaction_attributes_on_interaction_id_and_key'
   end
