fe5b78ecaa45486e6d554029945743f4b5705bc3
Kate <katerina_karpycheva@mail.ru>
2017-04-10 19:54:06 +0300

    Changed user serializer

--- app/serializers/beach_api_core/simple_interaction_serializer.rb
+++ app/serializers/beach_api_core/simple_interaction_serializer.rb
@@ -4,7 +4,7 @@ module BeachApiCore
     acts_as_abs_doc_id
 
     attributes :id, :kind, :created_at
-    belongs_to :user
+    belongs_to :user, serializer: BeachApiCore::UserSimpleSerializer
     has_many :interaction_keepers
     has_many :interaction_attributes
   end
