6ea20e9d1fb13494c84655d63a08b8bbecfa6158
andy <mailto.alekseenko@gmail.com>
2017-04-19 13:21:20 +0000

    Prettified setting update interactor

--- app/interactors/beach_api_core/setting_update.rb
+++ app/interactors/beach_api_core/setting_update.rb
@@ -1,20 +1,17 @@
 class BeachApiCore::SettingUpdate
   include Interactor
 
+  before do
+    context.setting = BeachApiCore::Setting.find_or_create_by(name: context.name,
+                                                              keeper: context.keeper)
+  end
+
   def call
-    context.setting = setting
     if context.setting.update context.params
       context.status = :ok
     else
       context.status = :bad_request
       context.fail! message: context.setting.errors.full_messages
     end
-  end
-
-  private
-
-  def setting
-    BeachApiCore::Setting.find_or_create_by(name: context.name,
-                                            keeper: context.keeper)
   end
 end
