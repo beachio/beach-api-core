228cc20c85f54af28df0472e9d3b95a61613400c
andy <mailto.alekseenko@gmail.com>
2017-04-24 16:18:28 +0000

    Updated email api docs

--- app/docs/emails_doc.rb
+++ app/docs/emails_doc.rb
@@ -4,13 +4,13 @@ module EmailsDoc
 
   api :POST, '/emails', 'Send an email'
   param :email, Hash, required: true do
-    param :user_ids, Array
-    param :from, String
-    param :to, String
+    param :user_ids, Array, desc: 'An array of user ids to send email instead of `to` param'
+    param :from, String, desc: 'Sender email'
+    param :to, String, desc: 'Recipient emails joined by comma'
     param :cc, String
     param :subject, String
     param :body, String
-    param :scheduled_time, Integer
+    param :scheduled_time, Integer, desc: 'Timestamp for sending out an email'
   end
   header 'AUTHORIZATION', 'application_id application_uid, client_secret application_secret', required: true
   def create
