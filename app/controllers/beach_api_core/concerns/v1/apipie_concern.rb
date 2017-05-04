61d3c90edd55d79b598100fb1018f3acdf920744
andy <mailto.alekseenko@gmail.com>
2017-05-04 10:39:36 +0000

    Updated projects docs

--- app/controllers/beach_api_core/concerns/v1/apipie_concern.rb
+++ app/controllers/beach_api_core/concerns/v1/apipie_concern.rb
@@ -112,7 +112,13 @@ module BeachApiCore::Concerns::V1::ApipieConcern
 
   def apipie_project
     @_apipie_project ||= BeachApiCore::Project.new(id: fake_id,
-                                                   name: Faker::Name.title)
+                                                   name: Faker::Name.title,
+                                                   project_keepers: [apipie_project_keeper])
+  end
+
+  def apipie_project_keeper
+    @_apipie_project_keeper ||= BeachApiCore::ProjectKeeper.new(id: fake_id,
+                                                                keeper: apipie_instance)
   end
 
   def pretty(serializer)
--- app/docs/projects_doc.rb
+++ app/docs/projects_doc.rb
@@ -5,9 +5,38 @@ module ProjectsDoc
   api :POST, '/projects', 'Create a new project'
   param :project, Hash, required: true do
     param :name, String, required: true
+    param :project_keepers_attributes, Array do
+      param :keeper_id, Integer
+      param :keeper_type, String
+    end
   end
   header 'HTTP_AUTHORIZATION', 'Bearer access_token', required: true
   example "\"project\": #{apipie_project_response}"
   def create
+  end
+
+  api :GET, '/projects/:id', 'Get owned project info'
+  header 'HTTP_AUTHORIZATION', 'Bearer access_token', required: true
+  example "\"project\": #{apipie_project_response}"
+  def show
+  end
+
+  api :PUT, '/projects/:id', 'Update an existing owned project'
+  param :project, Hash, required: true do
+    param :name, String, required: true
+    param :project_keepers_attributes, Array do
+      param :keeper_id, Integer
+      param :keeper_type, String
+    end
+  end
+  header 'HTTP_AUTHORIZATION', 'Bearer access_token', required: true
+  example "\"project\": #{apipie_project_response}"
+  def update
+  end
+
+  api :DELETE, '/projects/:id', 'Destroy an existing owned project'
+  header 'HTTP_AUTHORIZATION', 'Bearer access_token', required: true
+  example "\"project\": #{apipie_project_response}"
+  def destroy
   end
 end
