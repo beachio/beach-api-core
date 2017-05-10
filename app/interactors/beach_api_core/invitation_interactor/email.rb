872391c1ceed3c48ca4878bc14924897fdfed3de
Kate <katerina_karpycheva@mail.ru>
2017-05-10 14:32:09 +0300

    Fixed specs

--- app/interactors/beach_api_core/invitation_interactor/email.rb
+++ app/interactors/beach_api_core/invitation_interactor/email.rb
@@ -3,7 +3,7 @@ module BeachApiCore::InvitationInteractor
     include Interactor
 
     def call
-      if context.invitation.group.is_a?(Team) || context.invitation.group.is_a?(Organisation)
+      if context.invitation.group.is_a?(BeachApiCore::Team) || context.invitation.group.is_a?(BeachApiCore::Organisation)
         BeachApiCore::InvitationMailer.group_invite(context.invitation).deliver_now
       end
     end
