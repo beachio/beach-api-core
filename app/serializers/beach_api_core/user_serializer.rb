4850829b17f349659475e3a8671bb3e1fe888e3e
Kate <katerina_karpycheva@mail.ru>
2017-05-10 16:49:40 +0300

    Small change

--- app/serializers/beach_api_core/user_serializer.rb
+++ app/serializers/beach_api_core/user_serializer.rb
@@ -3,18 +3,13 @@ module BeachApiCore
     include BeachApiCore::Concerns::DocIdAbsSerializerConcern
     acts_as_abs_doc_id
 
-    attributes :id, :email, :username, :user_preferences, :confirmation_token
+    attributes :id, :email, :username, :user_preferences
     has_one :profile, serializer: BeachApiCore::ProfileSerializer
     has_many :organisations
 
     def user_preferences
       return [] unless application
       object.user_preferences.where(application: application)
-    end
-
-    def confirmation_token
-      return if object.confirmed?
-      object.confirm_email_token
     end
 
     private
