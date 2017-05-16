565e90c4566d24baeb1615f83c19782613b0b4be
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2017-05-16 16:50:42 +0000

    Add is_me flag to UserSerializer

--- app/serializers/beach_api_core/user_serializer.rb
+++ app/serializers/beach_api_core/user_serializer.rb
@@ -3,13 +3,17 @@ module BeachApiCore
     include BeachApiCore::Concerns::DocIdAbsSerializerConcern
     acts_as_abs_doc_id
 
-    attributes :id, :email, :username, :user_preferences
+    attributes :id, :email, :username, :user_preferences, :is_me
     has_one :profile, serializer: BeachApiCore::ProfileSerializer
     has_many :organisations
 
     def user_preferences
       return [] unless application
       object.user_preferences.where(application: application)
+    end
+
+    def is_me
+      object == instance_options[:current_user]
     end
 
     private
