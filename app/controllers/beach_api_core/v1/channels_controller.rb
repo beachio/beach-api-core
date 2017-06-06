3598466d2d4a0f391d5910452f6c0b327e2ab94a
Max Hordiichuk <hordijchuk.m.i@gmail.com>
2017-06-06 15:02:15 +0300

    Refactor channels controller

--- app/controllers/beach_api_core/v1/channels_controller.rb
+++ app/controllers/beach_api_core/v1/channels_controller.rb
@@ -2,6 +2,7 @@ module BeachApiCore
   class V1::ChannelsController < BeachApiCore::V1::BaseController
     include ChannelsDoc
     before_action :doorkeeper_authorize!
+    before_action :ensure_entity_params
 
     resource_description do
       name I18n.t('activerecord.models.beach_api_core/channel.other')
@@ -10,16 +11,13 @@ module BeachApiCore
     end
 
     def index
-      if params[:entity].present? && !(params[:entity][:uid] && params[:entity][:kind])
-        return render_json_error({ message: I18n.t('api.errors.some_parameters_are_absent') }, :bad_request)
-      end
       render_json_success({ channels: channels }, :ok)
     end
 
     private
 
     def channels
-      [user_channel, entity_channel].reject(&:empty?)
+      [user_channel, entity_channel].compact
     end
 
     def user_channel
@@ -28,13 +26,19 @@ module BeachApiCore
 
     def entity_channel
       return @_entity_channel if @_entity_channel
-      return {} unless params[:entity]
+      return unless params[:entity]
       entity ||= Entity.find_by(entity_lookup_params)
-      @_entity_channel = entity ? { channel: 'EntityChannel', id: entity.id } : {}
+      @_entity_channel = entity ? { channel: 'EntityChannel', id: entity.id } : nil
     end
 
     def entity_lookup_params
       params.require(:entity).permit(:uid, :kind).merge(user: current_user)
+    end
+
+    def ensure_entity_params
+      if params[:entity].present? && !(params[:entity][:uid] && params[:entity][:kind])
+        render_json_error({ message: I18n.t('api.errors.some_parameters_are_absent') }, :bad_request)
+      end
     end
   end
 end
