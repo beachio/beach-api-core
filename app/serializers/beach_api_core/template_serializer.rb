bd5381c56cc994a92bb9a2270cc1547550b38c04
Max Hordiichuk <hordijchuk.m.i@gmail.com>
2017-06-09 12:59:12 +0300

    Revert "Use OptionSerializerConcern in TemplateSerializer"
    This reverts commit e60e4773d10a8c5f6e2c5bb095aca46d8a41a6de.

--- app/serializers/beach_api_core/template_serializer.rb
+++ app/serializers/beach_api_core/template_serializer.rb
@@ -1,7 +1,5 @@
 module BeachApiCore
   class TemplateSerializer < ActiveModel::Serializer
-    include BeachApiCore::Concerns::OptionSerializerConcern
-    acts_with_options(:interaction)
     attributes :value
 
     def value
