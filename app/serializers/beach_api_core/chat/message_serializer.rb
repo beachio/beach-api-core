771ebfce3e4fc68c3c8948bf2bbd2da69d69a711
Max Hordiichuk <hordijchuk.m.i@gmail.com>
2017-06-09 11:36:46 +0300

    Use OptionSerializerConcern in MessageSerializer

--- app/serializers/beach_api_core/chat/message_serializer.rb
+++ app/serializers/beach_api_core/chat/message_serializer.rb
@@ -1,7 +1,9 @@
 module BeachApiCore
   class Chat::MessageSerializer < ActiveModel::Serializer
     include BeachApiCore::Concerns::DocIdAbsSerializerConcern
+    include BeachApiCore::Concerns::OptionSerializerConcern
     acts_as_abs_doc_id(:id)
+    acts_with_options :current_user
 
     attributes :id, :message, :read
 
@@ -9,12 +11,6 @@ module BeachApiCore
 
     def read
       object.read(current_user) if current_user
-    end
-
-    private
-
-    def current_user
-      instance_options[:current_user]
     end
   end
 end
