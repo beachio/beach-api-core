e8f5f8179582772ea1637208f539b84087dc3528
Max Hordiichuk <hordijchuk.m.i@gmail.com>
2017-06-12 13:23:54 +0300

    Improve template serializer. Fix tests

--- app/serializers/beach_api_core/template_serializer.rb
+++ app/serializers/beach_api_core/template_serializer.rb
@@ -1,13 +1,11 @@
 module BeachApiCore
   class TemplateSerializer < ActiveModel::Serializer
+    include BeachApiCore::Concerns::OptionSerializerConcern
+    acts_with_options(:interaction)
     attributes :value
 
     def value
       object.decorate.pretty_value(interaction)
-    end
-
-    def interaction
-      instance_options[:interaction]
     end
   end
 end
