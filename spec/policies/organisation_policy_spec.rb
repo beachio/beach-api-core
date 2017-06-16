157547cf5b95a382461fb607d9958151a728f991
Kate <katerina_karpycheva@mail.ru>
2017-06-16 20:50:45 +0300

    Allowed admin to get org users

--- spec/policies/organisation_policy_spec.rb
+++ spec/policies/organisation_policy_spec.rb
@@ -6,10 +6,17 @@ describe BeachApiCore::OrganisationPolicy do
   let!(:organisation) { create :organisation }
   let!(:owner) { (create :membership, group: organisation, owner: true).member }
   let!(:user) { (create :membership, group: organisation).member }
+  let!(:admin) { (create :membership, group: organisation).member }
+
+  before do
+    role = create :role, name: 'admin'
+    create :assignment, role: role, keeper: organisation, user: admin
+  end
 
   %i(update? destroy?).each do |action|
     permissions action do
       it { is_expected.to permit(owner, organisation) }
+      it { is_expected.to permit(admin, organisation) }
       it { is_expected.to_not permit(user, organisation) }
     end
   end
