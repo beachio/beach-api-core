cdeaa828441fc7053cafa90df9107724bb727b89
Max Hordiichuk <hordijchuk.m.i@gmail.com>
2017-06-23 13:19:33 +0300

    Improve Controller queries

--- app/controllers/beach_api_core/concerns/action_permissions_concern.rb
+++ app/controllers/beach_api_core/concerns/action_permissions_concern.rb
@@ -8,16 +8,11 @@ module BeachApiCore::Concerns::ActionPermissionsConcern
       return unless current_application
       controller_classname = self.class.name
 
-      controllers_with_actions =
-        BeachApiCore::Controller.joins(:actions).where(name: controller_classname,
-                                                       beach_api_core_actions: { name: action_name }).select(:id)
-      controllers_without_actions =
-        BeachApiCore::Controller.left_outer_joins(:actions).group(:id, :name)
-                                .having(name: controller_classname)
-                                .having('COUNT(beach_api_core_actions.*) = 0').select(:id)
+      controllers_with_action = BeachApiCore::Controller.with_action(controller_classname, action_name).select(:id)
+      controllers_without_actions = BeachApiCore::Controller.without_actions(controller_classname).select(:id)
 
-      has_action_permissions = current_application.services.joins(:controllers).where.has do |services|
-        services.id.in(controllers_with_actions) | services.id.in(controllers_without_actions)
+      has_action_permissions = current_application.services.joins(:controllers).where.has do
+        controllers.id.in(controllers_with_action) | controllers.id.in(controllers_without_actions)
       end.exists?
 
       return true if has_action_permissions
--- app/models/beach_api_core/controller.rb
+++ app/models/beach_api_core/controller.rb
@@ -4,6 +4,14 @@ module BeachApiCore
     has_many :actions, dependent: :destroy
     accepts_nested_attributes_for :actions, allow_destroy: true
 
+    scope(:with_action, lambda do |controller_classname, action_name|
+      joins(:actions).where.has { (name == controller_classname) & (actions.name == action_name) }
+    end)
+
+    scope(:without_actions, lambda do |controller_classname|
+      left_outer_joins(:actions).where(name: controller_classname).group(:id).when_having { actions.id.count == 0 }
+    end)
+
     validates :name, :service, presence: true
   end
 end
