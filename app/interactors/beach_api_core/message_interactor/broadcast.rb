bccb7cc129171ababcbda9beca83f262513cc929
Max Hordiichuk <hordijchuk.m.i@gmail.com>
2017-06-27 15:01:02 +0300

    Broadcast message text

--- app/interactors/beach_api_core/message_interactor/broadcast.rb
+++ app/interactors/beach_api_core/message_interactor/broadcast.rb
@@ -3,7 +3,9 @@ module BeachApiCore::MessageInteractor
     include Interactor
 
     def call
-      BeachApiCore::UserChannel.broadcast_to(context.current_user, 'user' => context.current_user)
+      BeachApiCore::UserChannel.broadcast_to(context.current_user,
+                                             user: context.current_user,
+                                             payload: { message: context.message.message })
       context.status = :ok
     end
   end
