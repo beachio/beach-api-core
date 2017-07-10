4ac02b5dd89f7940b59c9784a221cdfda6ea21ca
Max Hordiichuk <hordijchuk.m.i@gmail.com>
2017-07-10 15:27:07 +0300

    Broadcast chat message to all users in the chat

--- app/interactors/beach_api_core/message_interactor/broadcast.rb
+++ app/interactors/beach_api_core/message_interactor/broadcast.rb
@@ -3,9 +3,9 @@ module BeachApiCore::MessageInteractor
     include Interactor
 
     def call
-      BeachApiCore::UserChannel.broadcast_to(context.current_user,
-                                             user: context.current_user,
-                                             payload: { message: context.message.message })
+      context.chat.users.each do |user|
+        BeachApiCore::UserChannel.broadcast_to(user, user: user, payload: { message: context.message.message })
+      end
       context.status = :ok
     end
   end
