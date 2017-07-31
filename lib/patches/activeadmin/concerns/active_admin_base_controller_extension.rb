c315288957b4d501274b4907da505c00a2dfc76c
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2017-07-31 07:59:25 +0000

    Add AA extension

---
+++ lib/patches/activeadmin/concerns/active_admin_base_controller_extension.rb
@@ -0,0 +1,25 @@
+module ActiveAdminBaseControllerExtension
+  extend ActiveSupport::Concern
+
+  included do
+    def authenticate_admin_user!
+      return redirect_to(admin_sessions_new_path) unless user_signed_in?
+      return if current_user.role?(BeachApiCore::Role.admin)
+      session[:user_id] = nil
+      flash[:alert] = I18n.t('errors.not_authorized')
+      redirect_to admin_root_path
+    end
+
+    def user_signed_in?
+      session[:user_id].present?
+    end
+
+    def current_user
+      @current_user ||= BeachApiCore::User.find_by(id: session[:user_id])
+    end
+
+    def access_denied(_e)
+      redirect_to(admin_sessions_new_path)
+    end
+  end
+end
