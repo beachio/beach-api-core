db488b2175326b621cf5ff96cd628015a9c41858
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2017-08-01 17:42:05 +0000

    Added AA helpers

---
+++ app/helpers/beach_api_core/admin/application_helper.rb
@@ -0,0 +1,28 @@
+module BeachApiCore::Admin::ApplicationHelper
+  def keepers
+    keepers = Doorkeeper::Application.all.to_a.concat(BeachApiCore::Organisation.all)
+    keepers.push(BeachApiCore::Instance.current)
+    keepers.map { |keeper| [keeper_name(keeper), "#{keeper.class}##{keeper.id}"] }
+  end
+
+  def keeper_name(keeper)
+    case keeper.class.to_s
+      when 'BeachApiCore::Instance' then 'Current Instance'
+      else
+        keeper.class.to_s =~ /(.+)::(.+)/
+        "#{Regexp.last_match(2)} - #{keeper.name} (#{keeper.id})"
+    end
+  end
+
+  def value_for_user(user, field)
+    BeachApiCore::ProfileAttribute.find_by(profile: user.profile, profile_custom_field: field)&.value || ''
+  end
+
+  def status_options(status)
+    %w(Yes No Unsure).inject([]) do |acc, elem|
+      option = [elem, elem]
+      option << { checked: 'checked' } if elem == status
+      acc << option
+    end
+  end
+end
