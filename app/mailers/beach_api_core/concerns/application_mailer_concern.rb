9f7a7b2760cce500dc878c6ca7cd59e1875b5c05
Kate <katerina_karpycheva@mail.ru>
2017-10-26 15:47:37 +0000

    Added missing reset pass token

--- app/mailers/beach_api_core/concerns/application_mailer_concern.rb
+++ app/mailers/beach_api_core/concerns/application_mailer_concern.rb
@@ -8,8 +8,9 @@ module BeachApiCore::Concerns::ApplicationMailerConcern
       BeachApiCore::Setting.send(kind, keeper: @application)
     end
 
-    def client_url(path)
-      URI.join(BeachApiCore::Setting.client_domain(keeper: @application), path).to_s
+    def client_url(path, query = {})
+      URI.join(BeachApiCore::Setting.client_domain(keeper: @application), path,
+               "#{'?' if query.present?}#{URI.encode_www_form(query)}").to_s
     end
   end
 end
