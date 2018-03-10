3540e9391f5ebf3bf6fddb71b5c4f473f7a50a39
Paul Kononenko <storuky@gmail.com>
2018-03-10 17:17:19 +0300

    update form

--- app/models/beach_api_core/concerns/screenable.rb
+++ app/models/beach_api_core/concerns/screenable.rb
@@ -2,7 +2,7 @@ module BeachApiCore::Concerns::Screenable
   extend ActiveSupport::Concern
 
   included do
-    has_many :screens, class_name: "BeachApiCore::Screen", as: :resource
+    has_many :screens, ->{order(:position)}, class_name: "BeachApiCore::Screen", as: :resource
     accepts_nested_attributes_for :screens
   end
 end
