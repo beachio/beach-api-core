3035ad972bffd78bc4546b7ffafbcc20da9686cd
Paul Kononenko <storuky@gmail.com>
2018-03-16 16:01:50 +0300

    add mobile-card

---
+++ app/assets/javascripts/screens/components/mobile-card/mobile-card.js
@@ -0,0 +1,23 @@
+app.directive('mobileCard', [function(){
+  // Runs during compile
+  return {
+    // name: '',
+    // priority: 1,
+    // terminal: true,
+    scope: {
+      settings: "="
+    }, // {} = isolate, true = child, false/undefined = no change
+    // controller: function($scope, $element, $attrs, $transclude) {},
+    // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+    // restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+    require: '^screen',
+    // template: '',
+    templateUrl: 'mobile-card.html',
+    // replace: true,
+    // transclude: true,
+    // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+    link: function($scope, iElm, iAttrs, controller) {
+      $scope.screen = controller.screen;
+    }
+  };
+}]);
