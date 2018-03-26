bc68b2b8ba420fc7136cd267671d24fa69b2354f
Paul Kononenko <storuky@gmail.com>
2018-03-27 01:37:22 +0300

    template loaded by default

--- app/assets/javascripts/active_admin/screens/app.js
+++ app/assets/javascripts/active_admin/screens/app.js
@@ -8,6 +8,7 @@
 
 var app = angular.module("app", ['slick', 'ngResource', 'gridster', "ui.sortable", "checklist-model", "ngDialog", "ngMaterial"])
 
-app.run(['$http', function($http){
+app.run(['$http', 'Template', function($http, Template){
   $http.defaults.headers.common.Authorization = 'Bearer 39a4680f9f6aee65cc94b49d5e81fc866e2fe15a6fd04af79eac9cdf99d035a3';
+  Template.loaded = true;
 }])
