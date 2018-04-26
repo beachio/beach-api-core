e102487d879b3063d4cf3630749a3f5d61196f96
Paul Kononenko <storuky@gmail.com>
2018-04-26 19:43:13 +0300

    fix app.js

--- app/assets/javascripts/active_admin/screens/app.js
+++ app/assets/javascripts/active_admin/screens/app.js
@@ -6,6 +6,7 @@
 var app = angular.module("app", ['slick', 'ngResource', 'gridster', "ui.sortable", "checklist-model", "ngDialog", "ngMaterial"])
 
 app.run(['$http', 'Template', function($http, Template){
-  $http.defaults.headers.common.Authorization = 'Bearer 6552bb812ffff1a3e13d858b4065be49aee48f8013245003339567400c1510bd';
   Template.loaded = true;
+  window.token = '4f626a6d50777a1e05b5e6763cd244b25bcc3b37ab47cd4265e9cbe950379b3e';
+  $http.defaults.headers.common.Authorization = 'Bearer ' + window.token;
 }])
