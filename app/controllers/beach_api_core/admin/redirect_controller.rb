ad68c6cb46da3997f11636772849006dda45bfe8
Paul Kononenko <storuky@gmail.com>
2018-05-07 00:35:14 +0300

    fix redirect

--- app/controllers/beach_api_core/admin/redirect_controller.rb
+++ app/controllers/beach_api_core/admin/redirect_controller.rb
@@ -4,10 +4,13 @@ module BeachApiCore
     before_action :authenticate_admin_user!
 
     def index
-      return redirect_to "/admin/users" if current_user&.admin?
-      return redirect_to "/admin/nutrients" if current_user&.scientist?
-      return redirect_to "/admin/flows" if current_user&.editor?
-      redirect_to "/admin/sessions/new"
+      if current_user
+        return redirect_to "/admin/users" if current_user.admin?
+        return redirect_to "/admin/nutrients" if current_user.scientist?
+        return redirect_to "/admin/flows" if current_user.editor?
+      else
+        return redirect_to "/admin/sessions/new"
+      end
     end
 
   end
