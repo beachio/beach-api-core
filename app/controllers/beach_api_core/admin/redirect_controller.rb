4c1472829a843213bf7ae43b9ada4328d842af9a
kirill <hallamahyn@gmail.com>
2018-05-08 17:04:29 +0300

    fix redirect by non roles users

--- app/controllers/beach_api_core/admin/redirect_controller.rb
+++ app/controllers/beach_api_core/admin/redirect_controller.rb
@@ -3,13 +3,10 @@ module BeachApiCore
   class Admin::RedirectController < BeachApiCore::ApplicationController
 
     def index
-      if current_user
-        return redirect_to "/admin/users" if current_user.admin?
-        return redirect_to "/admin/nutrients" if current_user.scientist?
-        return redirect_to "/admin/flows" if current_user.editor?
-      else
-        return redirect_to "/admin/sessions/new"
-      end
+      return redirect_to "/admin/users" if current_user&.admin?
+      return redirect_to "/admin/nutrients" if current_user&.scientist?
+      return redirect_to "/admin/flows" if current_user&.editor?
+      redirect_to "/admin/sessions/new"
     end
 
   end
