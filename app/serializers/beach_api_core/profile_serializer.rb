cb49caf7fb54efb402f7aa8bac478f1b52f6fef9
AlexanderDmitriev <alexanderdmv@gmail.com>
2018-05-14 18:22:53 +0300

    fix profile url

--- app/serializers/beach_api_core/profile_serializer.rb
+++ app/serializers/beach_api_core/profile_serializer.rb
@@ -18,7 +18,7 @@ module BeachApiCore
     end
 
     def avatar_url
-      BeachApiCore::User.find(object.user_id).profile.avatar.file_url rescue nil
+      object.profile.avatar.file_url rescue nil
     end
   end
 end
