470bf464df27343660e0abc0de62f628fda2ec4c
Lumandra <lumandra7@gmail.com>
2018-07-09 11:05:48 +0300

    Fixed problem with policy

--- app/policies/beach_api_core/o_auth_application_policy.rb
+++ app/policies/beach_api_core/o_auth_application_policy.rb
@@ -1,7 +1,7 @@
 module BeachApiCore
   class OAuthApplicationPolicy < ApplicationPolicy
     def show?
-      record.owner == user || ::Pundit.policy(user, Instance.current).developer_or_admin?
+      record.owner == user
     end
 
     def manage?
