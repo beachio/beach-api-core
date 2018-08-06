06ddc24bd028ff948131e2f3bf499f9125625a4c
Paul Kononenko <storuky@gmail.com>
2018-08-06 19:01:13 +0300

    new apearance

--- app/assets/javascripts/screens/controllers/screens_ctrl.js
+++ app/assets/javascripts/screens/controllers/screens_ctrl.js
@@ -3,24 +3,19 @@ app.controller('ScreensCtrl', ['$scope', 'Config', 'Screen', '$timeout', 'Messag
   $scope.Screen = Screen
   $scope.Message = Message
 
+  ctrl.sendMessage = (message) => {
+    Message.push({from: "user", template: message})
+    ctrl.message = ""
+  }
+
   Screen.flow({flow_id: Config.flow_id}, function (res) {
     Screen.push(res)
   })
 
   $scope.$watch('[Screen.list, Message.list]', function (active, old) {
-    postMessage()
-  }, true)
-  postMessage()
-
-  function postMessage () {
-    $timeout(function () {
-      document.body.scrollTop = document.body.scrollHeight
-
-      if (window.sourceEvent) {
-        window.sourceEvent.postMessage({
-          height: $('.app-layout').height()
-        }, window.originEvent);
-      }
+    $timeout(() => {
+      let el = $(".app-layout-body")
+      el.animate({ scrollTop: el.height() }, "slow");
     })
-  }
+  }, true)
 }])
--- app/assets/javascripts/screens/services/message.js
+++ app/assets/javascripts/screens/services/message.js
@@ -1,9 +1,32 @@
-app.service('Message', [function(){
+app.service('Message', ['$timeout', function($timeout){
   var Message = this;
 
   Message.list = []
 
   Message.push = (message) => {
-    Message.list.push(message)
+    if (message.from == 'bot') {
+      Message.addToTempList(message)
+    } else {
+      Message.list.push(message)
+    }
+  }
+
+  Message.tempList = []
+  Message.addToTempList = (message) => {
+    Message.tempList.push(message)
+    Message.extractFromTempList()
+  }
+
+  Message.extractFromTempList = () => {
+    if (Message.tempList.length && !Message.inProgress) {
+      Message.inProgress = true
+      $timeout(() => {
+          Message.list.push(Message.tempList.pop())
+          if (!Message.tempList.length) Message.inProgress = false
+          Message.extractFromTempList()
+      }, 1000)
+    } else {
+      Message.inProgress = false
+    }
   }
 }])
