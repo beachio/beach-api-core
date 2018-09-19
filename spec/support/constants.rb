831bcd099b32b1d785ffbec658785cea57b8a0c1
Lumandra <lumandra7@gmail.com>
2018-09-19 09:54:04 +0300

    Added is_owner for teams

--- spec/support/constants.rb
+++ spec/support/constants.rb
@@ -4,6 +4,6 @@ module BeachApiCore
   PROFILE_KEYS = %i(id first_name last_name birth_date sex time_zone avatar_url).freeze
   USER_KEYS = %i(id email username profile organisations user_preferences is_me roles scores teams).freeze
   USER_SIMPLE_KEYS = %i(id email username first_name last_name avatar_url).freeze
-  TEAM_KEYS = %i(id name current_user_roles).freeze
+  TEAM_KEYS = %i(id name is_owner current_user_roles).freeze
   INTERACTION_KEYS = %i(id created_at kind template user interaction_attributes interaction_keepers).freeze
 end
