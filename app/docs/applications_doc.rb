6b842c9950a6782fd0bb5d721fdd6fe8ceb0450d
Paul Kononenko <storuky@gmail.com>
2018-11-26 13:21:20 +0300

    Fix double definition scores_for_invite in doc, and add created_at to app_serializer

--- app/docs/applications_doc.rb
+++ app/docs/applications_doc.rb
@@ -3,18 +3,17 @@ module ApplicationsDoc
   extend BeachApiCore::Concerns::V1::ApipieResponseConcern
 
   api :GET, '/applications', I18n.t('api.resource_description.descriptions.applications.list')
-  header 'HTTP_AUTHORIZATION', 'Bearer access_token', required: true
+  header 'Authorization', 'Bearer access_token', required: true
   example "\"applications\": [#{apipie_application_response}, ...]"
   def index; end
 
   api :POST, '/applications', I18n.t('api.resource_description.descriptions.applications.create')
-  header 'HTTP_AUTHORIZATION', 'Bearer access_token', required: true
+  header 'Authorization', 'Bearer access_token', required: true
   param :application, Hash, required: true do
     param :name, String, required: true
     param :redirect_uri, String, required: true
     param :mail_type_band_color, String, required: false, desc: "valid CSS hex color code"
     param :mail_type_band_text_color, String, required: false, desc: "valid CSS hex color code"
-    param :scores_for_invite, Integer, required: false
     param :scores_for_invite, Integer, required: false
   end
   example "\"application\": #{apipie_application_response}
@@ -23,12 +22,12 @@ module ApplicationsDoc
   def create; end
 
   api :GET, '/applications/:id', I18n.t('api.resource_description.descriptions.applications.get')
-  header 'HTTP_AUTHORIZATION', 'Bearer access_token', required: true
+  header 'Authorization', 'Bearer access_token', required: true
   example "\"application\": #{apipie_application_response}"
   def show; end
 
   api :PUT, '/applications/:id', I18n.t('api.resource_description.descriptions.applications.update')
-  header 'HTTP_AUTHORIZATION', 'Bearer access_token', required: true
+  header 'Authorization', 'Bearer access_token', required: true
   param :application, Hash, required: true do
     param :name, String
     param :mail_type_band_color, String, required: false, desc: "valid CSS hex color code"
@@ -42,7 +41,7 @@ module ApplicationsDoc
   def update; end
 
   api :POST, '/applications/:id/upload_logo_url', "Update email logo url"
-  header 'HTTP_AUTHORIZATION', 'Bearer access_token', required: true
+  header 'Authorization', 'Bearer access_token', required: true
   param 'file', String, desc: "Attachment via formdata"
   example "\"application\": #{apipie_application_response}
           \n#{I18n.t('api.resource_description.fail',
@@ -50,7 +49,7 @@ module ApplicationsDoc
   def upload_logo_url; end
 
   api :DELETE, '/applications/:id', I18n.t('api.resource_description.descriptions.applications.delete')
-  header 'HTTP_AUTHORIZATION', 'Bearer access_token', required: true
+  header 'Authorization', 'Bearer access_token', required: true
   example I18n.t('api.resource_description.fail',
                  description: I18n.t('api.errors.could_not_remove',
                                      model: I18n.t('activerecord.models.doorkeeper/application.downcase')))
