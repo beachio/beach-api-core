0fa3593301cc1ddc15b2d84c76e2a2899e26b42a
Denis Rozenkin <rozenkin@gmail.com>
2020-08-18 17:59:29 +0300

    Add only factories folder path

--- lib/beach_api_core/factories.rb
+++ lib/beach_api_core/factories.rb
@@ -1,6 +1,6 @@
 module BeachApiCore
   module Factories
     ENGINE_ROOT = File.dirname(File.dirname(File.dirname(__FILE__)))
-    PATH = Dir[File.join(ENGINE_ROOT, 'spec', 'factories', '*.rb')]
+    PATH = File.join(ENGINE_ROOT, 'spec', 'factories')
   end
 end
