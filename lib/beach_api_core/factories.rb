3017072c074c4206c08de54cdbd4ad898cdf2e4d
Denis Rozenkin <rozenkin@gmail.com>
2020-08-18 17:47:54 +0300

    Add factories path

--- lib/beach_api_core/factories.rb
+++ lib/beach_api_core/factories.rb
@@ -1,3 +1,6 @@
-ENGINE_ROOT = File.dirname(File.dirname(File.dirname(__FILE__)))
-
-Dir[File.join(ENGINE_ROOT, 'spec', 'factories', '*.rb')].each { |file| require(file) }
+module BeachApiCore
+  module Factories
+    ENGINE_ROOT = File.dirname(File.dirname(File.dirname(__FILE__)))
+    PATH = Dir[File.join(ENGINE_ROOT, 'spec', 'factories', '*.rb')]
+  end
+end
