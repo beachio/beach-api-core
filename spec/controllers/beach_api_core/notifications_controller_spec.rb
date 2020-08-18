c638db3cac04868d6f7015bf61e6e08a43cedae9
Rozan <rozanov61@gmail.com>
2020-08-18 16:52:34 +0300

    fixed notification tests

--- spec/controllers/beach_api_core/notifications_controller_spec.rb
+++ spec/controllers/beach_api_core/notifications_controller_spec.rb
@@ -4,19 +4,23 @@ module BeachApiCore
   RSpec.describe V1::NotificationsController, type: :controller do
     routes { BeachApiCore::Engine.routes }
 
-    describe "GET index" do
-      it "returns notification list" do
-        notification = BeachApiCore::Notification.create
+    include_context 'signed up developer'
+    include_context 'authenticated user'
+    include_context 'bearer token authentication'
+
+    describe 'GET index' do
+      it 'returns notification list' do
+        notification = BeachApiCore::Notification.create(user: oauth_user, kind: :ws)
         get :index
         expect(response.status).to eq(200)
-        expect(response.body).to include_json([{id: notification.id}])
+        expect(response.body).to include_json([{ id: notification.id }])
       end
     end
 
-    describe "DELETE destroy" do
-      it "delete notification" do
-        notification = Notification.create
-        delete :destroy, params: {id: notification.id}
+    describe 'DELETE destroy' do
+      it 'delete notification' do
+        notification = BeachApiCore::Notification.create(user: oauth_user, kind: :ws)
+        delete :destroy, params: { id: notification.id }
         expect(BeachApiCore::Notification.all).to be_empty
       end
     end
