9fbeed588e3eff84414c9172af914efa85677449
Rozan <rozanov61@gmail.com>
2020-09-10 17:54:45 +0300

    Backend: refactoring daily notifications

--- app/models/beach_api_core/notification.rb
+++ app/models/beach_api_core/notification.rb
@@ -4,26 +4,27 @@ module BeachApiCore
     belongs_to :project, class_name: 'VulcanCore::Project'
     enum kind: { email: 0, ws: 1 }
     enum notify_type: {
-      create_proj: 0,
-      update_proj: 1,
-      destroy_proj: 2,
+      create_project: 0,
+      update_project: 1,
+      destroy_project: 2,
       create_chart: 3,
       update_chart: 4,
       destroy_chart: 5,
       create_organisation: 6
     }
 
-    def dict
-      BeachApiCore::Notification
-        .where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
-        .where(notify_type: %i(update_proj destroy_proj create_chart update_chart destroy_chart))
-        .joins(%i(user project))
-        .group_by(&:user)
-        .map { |k, v| [k, v.group_by(&:project).map { |i, j| [i, j.group_by(&:notify_type).map { |a, b| [a, b.length] }.to_h]}.to_h]}
-        .to_h
-        .each do |user, dict|
-          BeachApiCore::NotificationMailer.with(user: user, dict: dict).daily_notifications.deliver_now
+    def self.mailing
+      self
+      .where(kind: 'email')
+      .joins(%i(user project))
+      .group_by(&:user).transform_values do |user|
+        user.group_by(&:project).transform_values do |project|
+          project.group_by(&:notify_type).transform_values &:length
         end
+      end
+      .each do |user, dict|
+        BeachApiCore::NotificationMailer.with(user: user, dict: dict).daily_notifications.deliver_now!
+      end
     end
   end
 end
--- lib/tasks/batch.rake
+++ lib/tasks/batch.rake
@@ -1,6 +1,6 @@
 namespace :batch do
   desc 'Send daily notify'
   task send_messages: :environment do
-    BeachApiCore::Notification.first.dict
+    BeachApiCore::Notification.mailing
   end
 end
